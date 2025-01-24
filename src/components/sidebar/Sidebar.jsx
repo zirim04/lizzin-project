a3c3fe891bf1d8f7822f30ce636c58fa4e956da0
Earnest Chukwuemeka <chukwuemeka.earnest@gmail.com>
2025-01-24 16:39:22 +0100

    updating my repo

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className="side_bar">
+      <div className="side_bar_top">
+        <div className="side_home">
+<img src="" alt="" srcset="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
